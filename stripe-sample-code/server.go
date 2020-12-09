ee85c42fbc7d95bb96489dc989beca71dacbaeb9
amikecallies <acallies15@apu.edu>
2020-12-09 11:48:48 -0800

    added template code for stripe package

---
+++ stripe-sample-code/server.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+  "bytes"
+  "encoding/json"
+  "io"
+  "log"
+  "net/http"
+
+  "github.com/stripe/stripe-go"
+  "github.com/stripe/stripe-go/paymentintent"
+)
+
+func main() {
+  // This is your real test secret API key.
+  stripe.Key = "sk_test_51HwHJ6LnR6D9WXz8pmhge55quL4f4MfHjjYKYxtYv8vC3fxXfjdzWp2e3bmG1RkXKMPzLWQTdIXEsqAfIFZ4acyD00jnSqMMtV"
+
+  fs := http.FileServer(http.Dir("."))
+  http.Handle("/", fs)
+  http.HandleFunc("/create-payment-intent", handleCreatePaymentIntent)
+
+  addr := "localhost:4242"
+  log.Printf("Listening on %s ...", addr)
+  log.Fatal(http.ListenAndServe(addr, nil))
+}
+
+type item struct {
+  id string
+}
+
+func calculateOrderAmount(items []item) int64 {
+  // Replace this constant with a calculation of the order's amount
+  // Calculate the order total on the server to prevent
+  // users from directly manipulating the amount on the client
+  return 1099
+}
+
+func handleCreatePaymentIntent(w http.ResponseWriter, r *http.Request) {
+  if r.Method != "POST" {
+    http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+    return
+  }
+
+  var req struct {
+    Items []item `json:"items"`
+  }
+
+  if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    log.Printf("json.NewDecoder.Decode: %v", err)
+    return
+  }
+
+  params := &stripe.PaymentIntentParams{
+    Amount:   stripe.Int64(calculateOrderAmount(req.Items)),
+    Currency: stripe.String(string(stripe.CurrencyUSD)),
+  }
+
+  pi, err := paymentintent.New(params)
+  log.Printf("pi.New: %v", pi.ClientSecret)
+
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    log.Printf("pi.New: %v", err)
+    return
+  }
+
+  writeJSON(w, struct {
+    ClientSecret string `json:"clientSecret"`
+  }{
+    ClientSecret: pi.ClientSecret,
+  })
+}
+
+func writeJSON(w http.ResponseWriter, v interface{}) {
+  var buf bytes.Buffer
+  if err := json.NewEncoder(&buf).Encode(v); err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+    log.Printf("json.NewEncoder.Encode: %v", err)
+    return
+  }
+  w.Header().Set("Content-Type", "application/json")
+  if _, err := io.Copy(w, &buf); err != nil {
+    log.Printf("io.Copy: %v", err)
+    return
+  }
+}
