977d025ff309a93614c48b894ab72133c0b426af
amikecallies <acallies15@apu.edu>
2025-01-19 08:48:58 -0800

    Update create-checkout-session.php

--- create-checkout-session.php
+++ create-checkout-session.php
@@ -7,8 +7,8 @@ require 'vendor/autoload.php';
 session_start();
 
 // $stripeSecret = getenv('STRIPE_SECRET');
-$amazonSesUserName = getenv('AMAZON_SES_SMTP_USERNAME');
-$amazonSesPassword = getenv('AMAZON_SES_SMTP_PASSWORD');
+// $amazonSesUserName = getenv('AMAZON_SES_SMTP_USERNAME');
+// $amazonSesPassword = getenv('AMAZON_SES_SMTP_PASSWORD');
 
 \Stripe\Stripe::setApiKey('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');
 
@@ -53,10 +53,10 @@ function sendMsgSeller($o_fullName, $o_email, $o_country, $o_address, $o_suite, 
   $mail->addAddress('manbehindmarch@gmail.com', 'Arlington Callies');
 
   // Replace smtp_username with your Amazon SES SMTP user name.
-  $mail->Username = $amazonSesUserName;
+  $mail->Username = 'AKIA4HJMF54KR6RMZO7G';
 
   // Replace smtp_password with your Amazon SES SMTP password.
-  $mail->Password = $amazonSesPassword;
+  $mail->Password = 'BIaPiubigPTFVZS6+vl90lygXYC4pn8XrkT++DfgaSkI';
       
   // Specify a configuration set. If you do not want to use a configuration
   // set, comment or remove the next line.
